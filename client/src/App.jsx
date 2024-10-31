879260c358549608b1ba43774417954b61c0081c
Yogesh Singh Chilwal <yogeshsinghchilwal@gmail.com>
2024-10-31 11:57:34 +0530

    Connect back end to front end

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
+import {useEffect, useState} from 'react'
 
 function App() {
+
+  const [message, setMessage] = useState("")
+
+  useEffect(() => {
+    async function getTodos(){
+      const res = await fetch("/api/todos")
+      const todos = await res.json()
+
+      console.log(todos);
+      setMessage(todos.mssg)
+      
+    }
+
+    getTodos()
+  }, [])
 
   return (
     <main className="container">
       <h1>Hello World!</h1>
+      <p style={{color: "red", fontSize: "2rem"}}>{message}</p>
     </main>
   )
 }
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+  },
 })
+
+/*
+server: { proxy: { '/api': 'http://localhost:5000' } }
+
+Configures the development server’s proxy.
+proxy: Defines a proxy rule where any request to /api (e.g., /api/users) will be forwarded to http://localhost:5000/api/users. 
+This setup is useful for avoiding CORS issues when your frontend (served on one port) communicates with a backend API (on another port).
+*/
