d4e8cde217f569a426c315b9b823c950981cc30f
Yogesh Singh Chilwal <yogeshsinghchilwal@gmail.com>
2024-11-04 16:09:03 +0530

    Updated get req

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, "dist")))
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "dist/index.html"));
 })
 
