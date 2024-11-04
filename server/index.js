6253241fab6b61d6713f2d7b5e90ad48c0cb272e
Yogesh Singh Chilwal <yogeshsinghchilwal@gmail.com>
2024-11-04 15:19:23 +0530

    Configured build

--- server/index.js
+++ server/index.js
@@ -2,9 +2,21 @@ import express from 'express'
 import router from './routes.js'
 import 'dotenv/config'
 import {connectToMongoDB} from './database.js'
+import path from 'path'
+import {fileURLToPath} from 'url'
 
 const app = express()
 app.use(express.json())
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "dist")))
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, "dist/index,html"));
+// })
+
 const port = process.env.PORT || 5000;
 
 app.use("/api", router)
