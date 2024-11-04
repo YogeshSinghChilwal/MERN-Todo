8ba6e92302c229c301af8e00501a5acf449e1d69
Yogesh Singh Chilwal <yogeshsinghchilwal@gmail.com>
2024-11-04 15:43:26 +0530

    added get req

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const __dirname = path.dirname(__filename);
 app.use(express.static(path.join(__dirname, "dist")))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, "dist/index,html"));
+    res.sendFile(path.join(__dirname, "dist/index.html"));
 })
 
 const port = process.env.PORT || 5000;
